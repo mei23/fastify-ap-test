20dff3a31f1694cf2c1879a3348d4f10fa8ff5d1
mei23 <m@m544.net>
2021-02-26 06:38:11 +0900

    some static

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,8 @@
 import * as Fastify from 'fastify';
 import fastifyStatic from 'fastify-static';
 import pointOfView from 'point-of-view';
+import favicon from 'fastify-favicon';
+
 const httpSignature = require('http-signature');
 import * as path from 'path';
 import * as pug from 'pug';
@@ -19,15 +21,31 @@ const server = Fastify.fastify({
 server.addContentTypeParser('application/activity+json', { parseAs: 'string' }, (server as any).getDefaultJsonParser('ignore', 'ignore'));
 server.addContentTypeParser('application/ld+json', { parseAs: 'string' }, (server as any).getDefaultJsonParser('ignore', 'ignore'));
 
+// 常にJSONとみなす場合は適切なプラグインスコープで以下 text/plainの既定のParserも上書きすること
+// server.addContentTypeParser('*', { parseAs: 'string' }, (server as any).getDefaultJsonParser('ignore', 'ignore'));
+
 const assetsDir = path.join(__dirname, '../../src/server/assets');
 
-// static dir
+// Assets directory
 server.register(fastifyStatic, {
 	root: assetsDir,
 	prefix: '/assets/',
 	maxAge: 3600 * 1000,
 });
 
+// Assets directory のものを / で配信したいときなど
+server.get('/apple-touch-icon.png', async (request, reply) => {
+	reply.sendFile('apple-touch-icon.png');
+});
+
+server.get('/robots.txt', async (request, reply) => {
+	reply.sendFile('robots.txt');
+});
+
+// favicon
+server.register(favicon, { path: `${assetsDir}` });
+
+// テンプレートエンジン
 server.register(pointOfView, {
 	engine: {
 		pug: pug
@@ -43,10 +61,11 @@ server.get('/test', async (request, reply) => {
 		});
 });
 
+// これはFastify的にはプラグイン機能って呼びます
 const api = async (server: Fastify.FastifyInstance, opts: Fastify.FastifyPluginOptions, done: (err?: Error) => void) => {
-	server.register(cors);	// scoped
+	server.register(cors);	// ちゃんとこのスコープのみに適用されます、えらい。
 
-	// default response header
+	// このプラグイン内では既定でこのヘッダーを付けてほしいってときに onRequestでもいいかも？
 	server.addHook('preHandler', async (request, reply) => {
 		reply
 			.header('Cache-Control', 'private, max-age=0, must-revalidate');
